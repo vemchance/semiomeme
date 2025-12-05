ba3d2bd56d13a1f4b2f4adc1eba7f77a1962f5b7
Victoria Sherratt <vem.chance@gmail.com>
2025-12-05 16:35:31 +0000

    Delete 04_Analysis/rando.py
    Deleting random file used to correct model hidden layer loading debug

--- 04_Analysis/rando.py
+++
@@ -1,9 +0,0 @@
-import torch
-ckpt = torch.load(r'X:\PhD\SemioMeme_Graph\data\corpus_data\text_model.pth', map_location='cpu')
-print(ckpt['config'])
-
-for key in ckpt['model_state_dict'].keys():
-    print(key)
-
-ckpt['config']['num_hidden_layers'] = 2
-torch.save(ckpt, r'X:\PhD\SemioMeme_Graph\data\corpus_data\text_model.pth')
